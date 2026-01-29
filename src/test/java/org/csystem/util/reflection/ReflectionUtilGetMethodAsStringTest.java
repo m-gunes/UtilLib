9a59b05fb8fd296179512f6ff45e50a7d49b75c2
m-gunes <mistafagunes@gmail.com>
2026-01-29 14:59:03 +0100

    refactor getMethodAsStringTest

--- src/test/java/org/csystem/util/reflection/ReflectionUtilGetMethodAsStringTest.java
+++ src/test/java/org/csystem/util/reflection/ReflectionUtilGetMethodAsStringTest.java
@@ -1,36 +1,39 @@
 package org.csystem.util.reflection;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.lang.reflect.Method;
 import java.util.Random;
 
 public class ReflectionUtilGetMethodAsStringTest {
+    private static int counter;
 
-    // Todo: bu testi ValueSource annotation ile yap
-    @Test
-    void test() throws Exception
+    @ParameterizedTest
+    @ValueSource(
+            strings = {
+                    "public int nextInt(int, int)",
+                    "public static double sqrt(double)",
+                    "public abstract void run()",
+                    "public void nextBytes(byte[])",
+                    "public java.util.stream.IntStream ints(long)",
+            }
+    )
+    void test(String str) throws Exception
     {
         var clsRandom = Random.class;
         var clsMath = Math.class;
         var clsRunnable = Runnable.class;
 
-        var method1 = clsRandom.getMethod("nextInt", int.class, int.class);
-        var method2 = clsMath.getMethod("sqrt", double.class);
-        var method3 = clsRunnable.getMethod("run");
-        var method4 = clsRandom.getMethod("nextBytes", byte[].class);
-        var method5 = clsRandom.getMethod("ints", long.class);
+        var methods = new Method[]{
+            clsRandom.getMethod("nextInt", int.class, int.class),
+            clsMath.getMethod("sqrt", double.class),
+            clsRunnable.getMethod("run"),
+            clsRandom.getMethod("nextBytes", byte[].class),
+            clsRandom.getMethod("ints", long.class),
+        };
 
-        var methodStr1Str = ReflectionUtil.getMethodPrototypesAsString(method1);
-        var methodStr2Str = ReflectionUtil.getMethodPrototypesAsString(method2);
-        var methodStr3Str = ReflectionUtil.getMethodPrototypesAsString(method3);
-        var methodStr4Str = ReflectionUtil.getMethodPrototypesAsString(method4);
-        var methodStr5Str = ReflectionUtil.getMethodPrototypesAsString(method5);
-
-        Assertions.assertEquals("public int nextInt(int, int)", methodStr1Str);
-        Assertions.assertEquals("public static double sqrt(double)", methodStr2Str);
-        Assertions.assertEquals("public abstract void run()", methodStr3Str);
-        Assertions.assertEquals("public void nextBytes(byte[])", methodStr4Str);
-        Assertions.assertEquals("public java.util.stream.IntStream ints(long)", methodStr5Str);
+        Assertions.assertEquals(str, ReflectionUtil.getMethodPrototypesAsString(methods[counter++]));
     }
 }
